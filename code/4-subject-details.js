f33a6c2a73979010334f707d9ebd2ccbffeef9c4
sophiep07 <parsonso14@craighead.school.nz>
2023-07-07 12:38:51 +1200

    done :)

--- code/4-subject-details.js
+++ code/4-subject-details.js
@@ -6,14 +6,21 @@ let subject = {
   name: 'DigiTech',
   room: 'Room 9',
   students: 150,
+  teacher: 'Random middle-aged man'
 }
 
 function showSubject() {
-  alert(subject.name)
+  alert(subject.name + ' in ' + subject.room)
 }
 
 function changeSubject() {
   let newName = 'Typing Skills'
 
   subject.name = newName
+}
+
+function changeTeacher() {
+  let newTeacher = prompt('Who is the new teacher?')
+
+  subject.name = newTeacher
 }
